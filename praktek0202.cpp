be55094a1fdfda40e90cf000611c65c5a724ca4f
anggirahmi <anggirahmi07@gmail.com>
2017-10-24 14:34:12 +0800

    autput dengan bermacam gaya

--- praktek0202.cpp
+++ praktek0202.cpp
@@ -9,5 +9,7 @@ int main(){
 	int niLAi=3;
 	
 	double angka(10.5),Angka,ANGKA(3.14) 
-	int a(10), b(7), c(12);	 	
-}
+	int a(10), b(7), c(12);	 
+	cout << nilai<<" : "<<Nilai<<" I "<<Nilai<<" "<<nilAi<<end;
+	cout << angka <<"/n"
+	cout << Angka <<end1<<ANGKA;
