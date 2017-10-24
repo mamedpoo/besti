9c3fbbce639b148d03a4e8ac688e7bf73e9da966
anggirahmi <anggirahmi07@gmail.com>
2017-10-24 15:37:44 +0800

    ouput hasil dari Jumlah Box Parket

--- Praktek0204.cpp
+++ Praktek0204.cpp
@@ -9,11 +9,11 @@ int main(){
 	double Luas1Box = LantaiPkt*8/1000000;
 	double JumlahBoxP, BiayaP, BiayaPG, totalB; 
 	
-	cout << "Luas Rumah			:"<<LuasRumah<<" m2"<<endl;
-	cout << "Luas 1 Box	Parket		: "<<Luas1Box<<" m2"<<endl;
+	cout << "Luas Rumah				:"<<LuasRumah<<" m2"<<endl;
+	cout << "Luas 1 Box	Parket			: "<<Luas1Box<<" m2"<<endl;
 	
 	JumlahBoxP = LuasRumah/Luas1Box;
-	
+	cout << "Jumlah Box Parket			: "	<<JumlahBoxP<<endl;	
 	
 	
 	
